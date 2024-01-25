70488ec1ce868a7e407a7cc919ba8f81b6bc57b6
riverhye <uhzigani@gmail.com>
2024-01-25 17:44:33 +0900

    rename

--- src/_04_class/_04_inheritance/ex1/Animal.java
+++ src/_04_class/_04_inheritance/ex1/Animal.java
@@ -6,11 +6,14 @@ public class Animal {
     private int age;
 
     public Animal(String species, String name, int age) {
+        // private 으로 선언했지만 같은 패키지라서 this 접근 가능
+        // 생성자로 초기화
         this.species = species;
         this.name = name;
         this.age = age;
     }
 
+    // 출력용 getter (변수가 모두 private 이므로)
     public String getName() {
         return name;
     }
--- src/_04_class/_04_inheritance/ex1/Cat.java
+++ src/_04_class/_04_inheritance/ex1/Cat.java
@@ -2,6 +2,8 @@ package _04_class._04_inheritance.ex1;
 
 class Cat extends Animal {
     public Cat(String name, int age) {
+        // Animal 클래스의 생성자 호출
+        // -> 매개변수가 있으므로 super 호출하여 초기화
         super("고양이", name, age);
     }
 
