2b4075871151c0912f9c89e53c15e503b34fd073
riverhye <uhzigani@gmail.com>
2024-01-27 18:00:49 +0900

    study: 코딩테스트2 하는중

---
+++ src/_08_test/Test2.java
@@ -0,0 +1,14 @@
+package _08_test;
+
+public class Test2 {
+    public static int[] findIndices(int[] nums, int targetNum) {
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums2 = {3, 2, 5, 7, 11};
+        int target2 = 9;
+        int[] result2 = findIndices(nums2, target2);
+        System.out.println("[" + result2[0] + ", " + result2[1] + "]");
+    }
+}
